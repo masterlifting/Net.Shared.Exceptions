715c97c4eebb6b29edbdc44a4391191c56c250b6
Andrey Pestunov <andreypestunov@gmail.com>
2023-02-08 01:57:47 +0100

    chore: recover code base

---
+++ src/Net.Shared.Exceptions/NetSharedException.cs
@@ -0,0 +1,17 @@
+﻿namespace Net.Shared.Exceptions;
+
+public abstract class NetSharedException : Exception
+{
+    private readonly string _message;
+
+    protected NetSharedException(string message)
+    {
+        _message = message;
+    }
+    protected NetSharedException(Exception exception)
+    {
+        _message = exception.InnerException?.Message ?? exception.Message;
+    }
+
+    public override string Message => _message;
+}
